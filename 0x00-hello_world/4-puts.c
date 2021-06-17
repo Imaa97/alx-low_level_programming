5b9158eb2f46ac3108c3cee58c25207906387215
Imaa Muheto <hopeimaa97@outlook.com>
2021-06-17 08:22:56 -0700

    printing using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point into the program
+ *
+ * Return: It should be a 0 success any other value is failure
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
