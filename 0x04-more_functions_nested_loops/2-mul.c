08b9543de42c9a817fabb267451b7e4e1628ea14
Imaa Muheto <hopeimaa97@outlook.com>
2021-06-24 02:06:35 -0700

    A funtion that multiplies two digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *mul(int a, int b)- multplies two numbers
+ *@a:a character in ascii
+ *@b:a character in ascii
+ *Return: result of the calculation
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
+
