6b86968586af40c0e815356e142d5bea4b97d4b0
Imaa Muheto <hopeimaa97@outlook.com>
2021-06-24 02:05:18 -0700

    A funtion that checks for digits 0 to 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *_isdigit - function checks for digits
+ *
+ *@c: a character in ascii
+ * Return: 1 if c is digit and 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+	return (1);
+	}
+
+	{
+	return (0);
+	}
+}
+
+
+
