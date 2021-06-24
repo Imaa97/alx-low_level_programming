a59f8f2ee00f02b8d8ccae31b3ccb935a1bf5d77
Imaa Muheto <hopeimaa97@outlook.com>
2021-06-24 02:34:36 -0700

    function that prints numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *print_numbers - 0 to 9
+ *
+ *
+ */
+void print_numbers(void)
+{
+	char i;
+
+	i = 0;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	_putchar(i);
+	}
+	{
+	_putchar('\n');
+	}
+}
