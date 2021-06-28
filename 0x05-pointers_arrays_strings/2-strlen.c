01ae078416c603a9b7a90aae9a183bbeabd8bd99
Imaa Muheto <hopeimaa97@outlook.com>
2021-06-28 03:42:57 -0700

    function that prints the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
- *int _strlen - returns length of a string
+ *_strlen - function for  length of a string
  *@s: a character in ASCII chart
- *
+ *Return: returns length of string
  */
 int _strlen(char *s)
 {
 	int len = 0;
-	
+
 	while (*s != '\0')
 	{
 		s++;
