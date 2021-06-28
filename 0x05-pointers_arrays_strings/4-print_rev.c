10754a0802352d70d155fa7e29cf3a5df94ed292
Imaa Muheto <hopeimaa97@outlook.com>
2021-06-28 07:15:38 -0700

    function that prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *print_rev - function to print in reverse
+ *@s: ASCII character to evaluate
+ *return: void
+ *
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
