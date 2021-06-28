a871157148b3a7adf1e4feba552be518bf8f1e5d
Imaa Muheto <hopeimaa97@outlook.com>
2021-06-28 00:26:03 -0700

    function that updates the value it points

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ *reset_to_98 - takes a integer and updates the value it points to.
+ *@n: a character in the ASCII chart
+ *Return: void no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
