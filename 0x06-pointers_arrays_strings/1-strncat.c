f6e4466af7c970fca0d3ed4b47ab8bd3cbc792bc
Imaa Muheto <hopeimaa97@outlook.com>
2021-07-01 01:28:50 -0700

    concatenates two strings using _strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ *_strncat -  appends n characters from array pointed by src
+ *to the end of string pointed by dest. Initial of src overwrites '\0'
+ *@src: pointer to source array
+ *@dest: pointer to the destination string
+ *Return: strncat reurns value of dest
+ *@n: maximum number of characters to be appended
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *start = dest;/*copy of dest string*/
+	/*pointer should not null pointer*/
+	if ((dest == 0) && (src == 0))
+	return (0);
+	/*find end of dest string*/
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	/**
+	 *Append src string characters until you do not
+	 *get null character of src or n != 0
+	 */
+	while (n--)
+	{
+		if (!(*dest++ = *src++))
+		{
+		return (start);
+		}
+	}
+	/*Append null charcater to the last digit of dest string*/
+	*dest = '\0';
+	return (start);
+}
