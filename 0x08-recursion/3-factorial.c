7b205fcedd4ce348a2289a6c89cbe0f3b64e47c6
Imaa Muheto <hopeimaa97@outlook.com>
2021-07-07 10:12:54 -0700

    factorial of a given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+/**
+ *factorial - function to print factorial of a number
+ *@n: value to find its  factorial
+ *Return: factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+
+}
