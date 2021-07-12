f762a1f2026642300275a40caca4c9d57dbf39b1
Imaa Muheto <hopeimaa97@outlook.com>
2021-07-12 11:03:17 -0700

    Added the comments, and changed the pointer sign

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ *main - entry point into the function
+ *@argc: argument counter
+ *@argv: array of pointers to arrays
+ *Return: 0 is always sucess any other number is failure
  */
-int main(int argc,char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
