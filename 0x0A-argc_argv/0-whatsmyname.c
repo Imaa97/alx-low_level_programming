9fb78cd7261d8d1909520c05932a9ae9b450c338
Imaa Muheto <hopeimaa97@outlook.com>
2021-07-12 05:44:39 -0700

    program prints its names a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,15 +3,16 @@
  *main - writes name of programe
  *@argc:argurment count
  *@argv: pointer to array of strings
- *return: o
+ *Return: zero always = sucess
  */
 int main(int argc, char *argv[])/*command line argurments*/
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s ", argv[i]);/*printing the string to stdout*/
 	}
 	printf("\n");/*printing a new line*/
-	return(0);
+	return (0);
 }
