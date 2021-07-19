d7b376202ef4d0af2dc2793058bc1fb06a92ce9c
Imaa Muheto <hopeimaa97@outlook.com>
2021-07-19 04:57:52 -0700

    A function-like macro that computes the absolute value of a number

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef ABS
+#define ABS absolute_value
+/**
+ *ABS - function that findds value of absolute value
+ *@x: number to evaluated
+ *Return: The computation within the parenthesis.
+ */
+int ABS(int x)
+{
+	return ((x < 0) ? (-x) : (x));
+}
+#endif
