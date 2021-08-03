ccb799ff19d2a919f392271cfd97a89867fcd15b
Imaa Muheto <hopeimaa97@outlook.com>
2021-08-03 00:58:52 -0700

    changed the function in entirety

--- 0x13-more_singly_linked_lists/3-add_nodeint.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 
 /**
- * add_nodeint - adds a new node at the beginning of a listint_t list.
- * @head: douuble pointer to the beginning of a listint_t list
+ * add_nodeint_end - adds a new node at the end of a listint_t list.
+ * @head: double pointer to the beginning of the list
  * @n: integer to add to the list
  *
- * Return: address of new node
+ * Return: pointer to the new node
  */
-listint_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
+	listint_t *current;
 
 	if (head == NULL)
 		return (NULL);
@@ -18,7 +19,17 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	new->next = *head;
-	*head = new;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	current = *head;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->next = new;
 	return (new);
 }
